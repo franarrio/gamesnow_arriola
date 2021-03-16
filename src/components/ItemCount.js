de864bb1ace395bb7ff41d71ec3129a8bb708d41
farriola <farriola@rightstuf.com>
2021-03-16 20:29:58 -0300

    games_now
    Crea un componente ItemCount.js que debe estar compuesto de un botón y controles para incrementar y decrementar la cantidad requerida de ítems.

---
+++ src/components/ItemCount.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { RiAddCircleFill, RiIndeterminateCircleFill } from 'react-icons/ri';
+import './ItemCount.css';
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+    const [count, setCount] = useState(initial < stock ? initial : 0);
+
+    useEffect(() => {
+        onAdd(count);
+    }, [count]);
+
+    return (
+        <div className="item-count">
+            <div className="input-group mb-3">
+                <button
+                    className="btn btn-outline-secondary"
+                    onClick={(e) => count < stock && setCount(count + 1)}
+                >
+                    <RiAddCircleFill />
+                </button>
+                <input
+                    type="quntity"
+                    className="form-control text-center"
+                    value={count}
+                    onChange={() => {}}
+                />
+                <button
+                    className="btn btn-outline-secondary"
+                    onClick={(e) => count > 0 && setCount(count - 1)}
+                >
+                    <RiIndeterminateCircleFill />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({ greeting }) => {
+    const onAdd = (count) => {
+        console.log('count', count);
+        console.log('onAdd ItemListContainer');
+    };
+
     return (
         <div className="container">
             <h3>{greeting}</h3>
-        </div>
+            <ItemCount stock={5} initial={0} onAdd={onAdd} />
+        </div> 
     );
 };
 
